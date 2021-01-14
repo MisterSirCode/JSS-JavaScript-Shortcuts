95b251b45ed7c6cb562910514cb41ca8d88406d7
Sir Code <44554976+MisterSirCode@users.noreply.github.com>
2021-01-14 08:45:56 -0500

    Update jss-main.js

--- jss-main.js
+++ jss-main.js
@@ -1,9 +1,9 @@
-Math.prototype.randWhole = function(min, max) { 
+Math.prototype.randInt = function(min, max) { 
     min = Math.ceil(min); 
     max = Math.floor(max); 
     return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
-Math.prototype.rand = function(min, max) {
+Math.prototype.randFloat = function(min, max) {
     return Math.random() * (max - min + 1) + min;
 }
