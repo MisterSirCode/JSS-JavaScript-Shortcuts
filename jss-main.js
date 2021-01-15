b8982a7fd0d253d73f61c6fe096d2037fb0ec032
Sir Code <44554976+MisterSirCode@users.noreply.github.com>
2021-01-15 08:32:31 -0500

    Update jss-main.js

--- jss-main.js
+++ jss-main.js
@@ -1,12 +1,29 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+// Returns a Random Integer between range
 Math.randInt = function(min, max) { 
     min = Math.ceil(min); 
     max = Math.floor(max); 
     return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
+// Returns a Random Float between range
 Math.randFloat = function(min, max) {
     return Math.random() * (max - min + 1) + min;
+}
+
+// Returns Cosecant of n
+Math.cosec = function(n) {
+    return 1 / this.sin(n);
+}
+
+// Returns Secant of n
+Math.sec = function(n) {
+    return 1 / this.cos(n)
+}
+
+// Returns Cotangent of n
+Math.cotan = function(n) {
+    return 1 / this.tan(n);
 }
